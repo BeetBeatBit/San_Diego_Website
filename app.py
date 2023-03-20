aa421cb990ce21c8c5b0665e726f0c0e532d0605
Daniel Carbajal <36678639+BeetBeatBit@users.noreply.github.com>
2023-03-19 21:59:10 -0600

    Seccion de comentarios, review.
    * Se añadio una sección de comentarios funcional, la cual lee y escribe datos de una base de datos.
    * Se añadio un div para el video de review que sigue el guión.

--- app.py
+++ app.py
@@ -1,13 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
+import database as db
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return redirect(url_for('mostrarComentarios'))
 
 @app.route("/index.html")
 def home():
-    return render_template("index.html")
+    return redirect(url_for('mostrarComentarios'))
+
+@app.route("/mostrarComments")
+def mostrarComentarios():
+    cursor = db.database.cursor()
+    cursor.execute("SELECT * FROM comments")
+    myresult = cursor.fetchall()
+    #Convertir los datos a diccionario
+    insertObject = []
+    columnNames = [column[0] for column in cursor.description]
+    for record in myresult:
+        insertObject.append(dict(zip(columnNames, record)))
+    cursor.close()
+    return render_template('index.html', data=insertObject)
+
+@app.route('/addComment', methods=['POST'])
+def addComments():
+     # Recupera los datos ingresados por el usuario en el input
+    fullname = request.form['full_name']
+    email = request.form['email']
+    comment = request.form['comment']
+
+    # Insertar el comentario en la base de datos
+    cursor = db.database.cursor()
+    # Insertar usuario nuevo dentro de la tabla users
+    sql = "INSERT INTO comments (fullname, email, comment) VALUES (%s, %s, %s);"
+    values = (fullname, email, comment)
+
+    # Ejecutar la consulta SQL y obtener los resultados
+    cursor.execute(sql, values)
+
+    if cursor.rowcount > 0:
+        db.database.commit()
+        #reg_success_msg = "Se registro el comentario exitosamente!"
+        return redirect(url_for('mostrarComentarios'))
+    else:
+        # Error al registrar usuario, mostrar mensaje de error al usuario
+        #reg_error_msg = "No se pudo registrar el comentario"
+        #return render_template('index.html', reg_error_msg=reg_error_msg)
+        print("No se pudo registar el comentario")
+        return redirect(url_for('mostrarComentarios'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True, port=4000, host="0.0.0.0")
---
+++ database.py
@@ -0,0 +1,8 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='Daniel98@',
+    database='san_diego_website'
+)
